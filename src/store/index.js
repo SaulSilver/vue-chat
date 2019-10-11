df1236f8a392b120a00480a90a50bc157f1ad985
Hatem <hatem@bentoandco.com>
2019-10-11 14:36:27 +0900

    Setting up data persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
+import mutations from "./mutations";
+import actions from "./actions";
 
 Vue.use(Vuex);
 
+const dev = process.env.NODE_ENV !== "production";
+
+const vuexLocal = new VuexPersistence({ storage: window.localStorage });
+
 export default new Vuex.Store({
   state: {},
-  mutations: {},
-  actions: {}
+  mutations,
+  actions,
+  getters: {},
+  plugins: [vuexLocal.plugin],
+  strict: dev
 });

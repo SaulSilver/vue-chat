9c53d112b70d2b8bf787d63710d8cd9d004976cf
Hatem <hatem@bentoandco.com>
2020-02-10 10:30:42 +0900

    Fix App layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="login card">
-    <header>
-      <h1>Vue.js Chat</h1>
-      <h4>{{$t('homepage.subtitle')}}</h4>
-      <a target="_blank" href="https://pusher.com/chatkit">{{ $t('homepage.more-info') }}</a>
-    </header>
+  <div class="login-page">
+    <div class="login card">
+      <header>
+        <h1>Vue.js Chat</h1>
+        <h4>{{$t('homepage.subtitle')}}</h4>
+        <a target="_blank" href="https://pusher.com/chatkit">{{ $t('homepage.more-info') }}</a>
+      </header>
 
-    <LoginForm></LoginForm>
-    <!-- TODO: add a language selector -->
+      <LoginForm></LoginForm>
+      <!-- TODO: add a language selector -->
+    </div>
   </div>
 </template>
 
